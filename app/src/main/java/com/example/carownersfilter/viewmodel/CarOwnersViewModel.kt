a7aa51ac7a8fa40ed6b87500a19e3ce9944523be
Bolu Okunaiya <Boluokunaiya@gmail.com>
2020-03-05 23:41:23 +0100

    Get File And Save FILE UI Complete

--- app/src/main/java/com/example/carownersfilter/viewmodel/CarOwnersViewModel.kt
+++ app/src/main/java/com/example/carownersfilter/viewmodel/CarOwnersViewModel.kt
@@ -1,13 +1,34 @@
 package com.example.carownersfilter.viewmodel
 
+import android.content.Context
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.carownersfilter.R
 import com.example.carownersfilter.repository.CarOwnerRepository
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class CarOwnersViewModel(private  val carOwnerRepository: CarOwnerRepository) :BaseViewModel(){
 
     fun getAllCarOwners() =  carOwnerRepository.getAllCarOwners().asLiveData()
+    val onDataSaveSuccessful = SingleLiveEvent<FileStatus>()
 
 
+     fun  getAndSaveCSV(context:Context){
+        viewModelScope.launch {
+            val csvData = withContext(IO){context.resources.openRawResource(R.raw.car_ownsers_data)}
+            val rows: List<List<String>> = withContext(IO){csvReader().readAll(csvData)}
+            println(rows[0])
+            carOwnerRepository.saveCSCData(rows)
+            onDataSaveSuccessful.value = FileStatus.FOUND
+        }
+    }
+
+
+}
+
+enum class FileStatus{
+    COULD_NOT_FIND,FOUND
 }
