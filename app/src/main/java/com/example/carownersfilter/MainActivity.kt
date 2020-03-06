f99fdc443b0454db6cedc17ad4f3d183b3adee8c
Bolu Okunaiya <Boluokunaiya@gmail.com>
2020-03-06 03:24:30 +0100

    My Filter Recycler

--- app/src/main/java/com/example/carownersfilter/MainActivity.kt
+++ app/src/main/java/com/example/carownersfilter/MainActivity.kt
@@ -4,12 +4,14 @@ package com.example.carownersfilter
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.carownersfilter.model.CarOwner
 import com.example.carownersfilter.repository.CarOwnerRepository
 import com.example.carownersfilter.repository.FilterCarOwnerHelper
 import com.example.carownersfilter.viewmodel.FiltersViewModel
 import org.koin.android.ext.android.inject
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,28 +24,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-
-//        val path = this.filesDir.absolutePath
-//        println(path)
-//       val fileDir =  this.getExternalFilesDir(null)
-//        println(fileDir?.absoluteFile)
-//
-//      // val file = File("$path/decagon/car_ownsers_data.csv")
-//
-//        carOwnerViewModel.getAllCarOwners().observeChange(this@MainActivity){
-//            carOwnersList = it
-//            filtersViewModel.getAllFiltersAPI()
-//        }
-//        filtersViewModel.allFiltersResponse.observeChange(this@MainActivity){
-//            Timber.d("FOR THESE FILTER ${it[1]} I GOT \n ${filterCarOwnerHelper.getAllCarOwnersForFilter(it[1],carOwnersList).takeLast(3)}")
-//        }
-
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.actionbar, menu)
         return true
+    }
+
+
+
+
+}
+fun NavController.navigateUpOrFinish(activity: AppCompatActivity): Boolean {
+    return if (navigateUp()) {
+        true
+    } else {
+        activity.finish()
+        true
     }
 }
--- app/src/main/java/com/example/carownersfilter/views/FilteredCarOwnersFragment.kt
+++ app/src/main/java/com/example/carownersfilter/views/FilteredCarOwnersFragment.kt
@@ -5,12 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.Group
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.carownersfilter.R
 import com.example.carownersfilter.model.Filters
+import com.example.carownersfilter.utils.loadImage
+import com.example.carownersfilter.utils.normalcase
+import com.example.carownersfilter.utils.updateRecycler2
+import kotlinx.android.synthetic.main.fragment_filtered_car_owners.*
+import kotlinx.android.synthetic.main.fragment_my_filters.*
 
 
 class FilteredCarOwnersFragment : BaseFragment() {
     lateinit var filter:Filters
+    private var adaptor: RecyclerView.Adapter<*>? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,6 +34,17 @@ class FilteredCarOwnersFragment : BaseFragment() {
         super.onViewCreated(view, savedInstanceState)
         arguments?.let {filter = FilteredCarOwnersFragmentArgs.fromBundle(it).filter }
         println(filter)
+        setUpRecycler()
+    }
+
+    private fun setUpRecycler() {
+        adaptor = recycler.updateRecycler2(context!!, listOf(1,2,3,4,56,67,7),R.layout.item_car_owner, listOf(),{ innerViews, item ->
+
+
+
+        },{item ->
+
+        })
     }
 
 
